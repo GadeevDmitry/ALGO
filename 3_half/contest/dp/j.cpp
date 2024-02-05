b611437f732a66505e2620bc972b3595988d2fb8
Gadeev Dmitry <gadeev.dima@mail.ru>
2024-02-06 01:27:46 +0300

    contest_1_task_J: fix code style

--- 3_half/contest/dp/j.cpp
+++ 3_half/contest/dp/j.cpp
@@ -1,71 +1,135 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <cstring>
 #include <vector>
-#include <string.h>
+
+//==================================================================================================
+
+static bool input(
+    char *s1, size_t *s1_len,
+    char *s2, size_t *s2_len);
+
+static bool str_scanf(char *s);
+
+static void output(
+    const size_t gcs_len,
+    const std::vector<int> &ans1,
+    const std::vector<int> &ans2);
+
+static size_t solve(
+    const char *s1, const size_t s1_len,
+    const char *s2, const size_t s2_len,
+
+    std::vector<int> &ans1,
+    std::vector<int> &ans2);
+
+//==================================================================================================
 
 int main()
 {
-    char s1[1000] = "";
-    char s2[1000] = "";
+    char s1[1001] = ""; size_t s1_len = 0;
+    char s2[1001] = ""; size_t s2_len = 0;
+    if (!input(s1, &s1_len, s2, &s2_len))
+        return 1;
 
-    scanf("%s %s", s1, s2);
+    std::vector<int> ans1;
+    std::vector<int> ans2;
+    size_t gcs_len = solve(s1, s1_len, s2, s2_len, ans1, ans2);
 
-    int n = strlen(s1);
-    int m = strlen(s2);
+    output(gcs_len, ans1, ans2);
+}
 
-    int dp[n + 1][m + 1];
-    std::vector<int> v1;
-    std::vector<int> v2;
+//--------------------------------------------------------------------------------------------------
 
-    for (int i = 0; i <= n; ++i)
+static bool input(
+    char *s1, size_t *s1_len,
+    char *s2, size_t *s2_len)
+{
+    if (!str_scanf(s1)) return false;
+    if (!str_scanf(s2)) return false;
+
+    *s1_len = strlen(s1);
+    *s2_len = strlen(s2);
+
+    return true;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+static bool str_scanf(char *s)
+{
+    int len = 0;
+    int res = scanf("%1001s%n", s, &len);
+
+    return res == 1 && len < 1001;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+static void output(
+    const size_t gcs_len,
+    const std::vector<int> &ans1,
+    const std::vector<int> &ans2)
+{
+    printf("%lu\n", gcs_len);
+
+    for (int i = 0; i < gcs_len; ++i)
+        printf("%d ", ans1[i]);
+    printf("\n");
+
+    for (int i = 0; i < gcs_len; ++i)
+        printf("%d ", ans2[i]);
+    printf("\n");
+}
+
+//--------------------------------------------------------------------------------------------------
+
+static size_t solve(
+    const char *s1, const size_t s1_len,
+    const char *s2, const size_t s2_len,
+
+    std::vector<int> &ans1,
+    std::vector<int> &ans2)
+{
+    int gcs[s1_len + 1][s2_len + 1];
+
+    for (int i = 0; i <= s1_len; ++i)
+        for (int j = 0; j <= s2_len; ++j)
+            gcs[i][j] = 0;
+
+    for (int i = 1; i <= s1_len; ++i)
     {
-        for (int j = 0; j <= m; ++j)
+        for (int j = 1; j <= s2_len; ++j)
         {
-            dp[i][j] = 0;
-        }
-    }
-
-    for (int i = 1; i <= n; ++i)
-    {
-        for (int j = 1; j <= m; ++j)
-        {
-            dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+            gcs[i][j] = std::max(gcs[i - 1][j], gcs[i][j - 1]);
 
             if (s1[i - 1] == s2[j - 1])
-                dp[i][j] = std::max(dp[i][j], dp[i - 1][j - 1] + 1);
+                gcs[i][j] = std::max(gcs[i][j], gcs[i - 1][j - 1] + 1);
         }
     }
 
-    int i = n;
-    int j = m;
+    int p1 = s1_len;
+    int p2 = s2_len;
 
-    while (i != 0 && j != 0)
+    while (p1 != 0 && p2 != 0)
     {
-        if ((dp[i][j] == dp[i - 1][j - 1] + 1) && (s1[i - 1] == s2[j - 1]))
+        if ((gcs[p1][p2] == gcs[p1 - 1][p2 - 1] + 1) && (s1[p1 - 1] == s2[p2 - 1]))
         {
-            v1.push_back(i);
-            v2.push_back(j);
+            ans1.push_back(p1);
+            ans2.push_back(p2);
 
-            --i;
-            --j;
+            --p1;
+            --p2;
         }
         else
         {
-            if (dp[i][j] == dp[i - 1][j]) --i;
-            else                          --j;
+            if (gcs[p1][p2] == gcs[p1 - 1][p2]) --p1;
+            else                                --p2;
         }
     }
 
-    std::reverse(v1.begin(), v1.end());
-    std::reverse(v2.begin(), v2.end());
+    std::reverse(ans1.begin(), ans1.end());
+    std::reverse(ans2.begin(), ans2.end());
 
-    printf("%d\n", dp[n][m]);
-
-    for (int i = 0; i < dp[n][m]; ++i)
-        printf("%d ", v1[i]);
-    printf("\n");
-
-    for (int i = 0; i < dp[n][m]; ++i)
-        printf("%d ", v2[i]);
-    printf("\n");
+    return gcs[s1_len][s2_len];
 }
